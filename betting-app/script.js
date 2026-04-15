7b24aa2c5929b9089f2ddd802016bccca3972237
11florin <albuflorin929@yahoo.co.uk>
2026-04-15 14:25:57 +0100

    Task completed

--- betting-app/script.js
+++ betting-app/script.js
@@ -40,3 +40,24 @@ const game = {
     team2: 6.5,
   },
 };
+
+const [players1, players2] = game.players;
+
+const [gk, ...fieldPlayers] = players1;
+
+const allPlayers = [...players1, ...players2];
+
+const players1Final = ['Thiago', 'Coutinho', 'Perisic', ...players1];
+
+const {team1, x: draw, team2} = game.odds;
+
+const printGoals = function (...players) {
+    // console.log(players);
+    console.log(`${players.length} goals were scored `);
+}
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
